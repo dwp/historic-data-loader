e97bb795f1c0d609040b52f9796170faf4978889
Daniel Chicot <dan.chicot@gmail.com>
2020-10-06 17:18:49 +0100

    First pass at unit migration.

---
+++ src/main/kotlin/app/load/mapreduce/MessageProducer.kt
@@ -0,0 +1,73 @@
+package app.load.mapreduce
+
+import app.load.domain.DataKeyResult
+import app.load.domain.EncryptionResult
+import com.google.gson.JsonObject
+import uk.gov.dwp.dataworks.logging.DataworksLogger
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MessageProducer {
+
+    fun produceMessage(jsonObject: JsonObject,
+                       id: String,
+                       idIsString: Boolean,
+                       idWasModified: Boolean,
+                       lastModifiedDateTime: String,
+                       lastModifiedDateTimeSourceKey: String,
+                       createdDateTimeWasModified: Boolean,
+                       removedDateTimeWasModified: Boolean,
+                       archivedDateTimeWasModified: Boolean,
+                       isRemovedRecord: Boolean,
+                       isArchivedRecord: Boolean,
+                       encryptionResult: EncryptionResult,
+                       dataKeyResult: DataKeyResult,
+                       database: String,
+                       collection: String): String {
+        val type = jsonObject.getAsJsonPrimitive("@type")?.asString ?: "MONGO_IMPORT"
+
+        val standardDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+        val timestamp = standardDateFormat.format(Date())
+        val messageId = if (idIsString) """"$id"""" else id
+        val lastModifiedDateTimeWasModified = lastModifiedDateTimeSourceKey != "_lastModifiedDateTime"
+
+        // TODO: "traceId": "$correlation_id",
+
+        return """{
+   "unitOfWorkId": "$unitOfWorkId",
+   "timestamp": "$timestamp",
+   "@type": "HDI",
+   "message": {
+       "@type": "$type",
+       "_id": $messageId,
+       "mongo_format_stripped_from_id": $idWasModified,
+       "last_modified_date_time_was_altered": $lastModifiedDateTimeWasModified,
+       "created_date_time_was_altered": $createdDateTimeWasModified,
+       "removed_date_time_was_altered": $removedDateTimeWasModified,
+       "archived_date_time_was_altered": $archivedDateTimeWasModified,
+       "historic_removed_record_altered_on_import": $isRemovedRecord,
+       "historic_archived_record_altered_on_import": $isArchivedRecord,
+       "_lastModifiedDateTime": "$lastModifiedDateTime",
+       "timestamp_created_from": "$lastModifiedDateTimeSourceKey",
+       "collection" : "$collection",
+       "db": "$database",
+       "dbObject": "${encryptionResult.encrypted}",
+       "encryption": {
+           "keyEncryptionKeyId": "${dataKeyResult.dataKeyEncryptionKeyId}",
+           "initialisationVector": "${encryptionResult.initialisationVector}",
+           "encryptedEncryptionKey": "${dataKeyResult.ciphertextDataKey}"
+       }
+   }
+}"""
+    }
+
+    companion object {
+        val logger = DataworksLogger.getLogger(MessageProducer::class.java.toString())
+    }
+
+
+    private val unitOfWorkId by lazy {
+        UUID.randomUUID().toString()
+    }
+
+}
---
+++ src/main/kotlin/app/load/mapreduce/MessageUtils.kt
@@ -0,0 +1,126 @@
+package app.load.mapreduce
+
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.beust.klaxon.lookup
+import com.google.gson.Gson
+import uk.gov.dwp.dataworks.logging.DataworksLogger
+import java.nio.ByteBuffer
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.zip.CRC32
+
+class MessageUtils {
+    val logger = DataworksLogger.getLogger(MessageUtils::class.java.toString())
+    val typeDefault = "MONGO_IMPORT"
+
+    fun parseGson(line: String): com.google.gson.JsonObject =
+        Gson().fromJson(line, com.google.gson.JsonObject::class.java)
+
+    @Throws(ParseException::class)
+    fun getTimestampAsLong(timeStampAsStr: String?): Long {
+        val validTimestamps = listOf(
+            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+            "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ"
+        )
+
+        validTimestamps.forEach {
+            try {
+                val df = SimpleDateFormat(it)
+                return df.parse(timeStampAsStr).time
+            }
+            catch (e: Exception) {
+
+            }
+        }
+        throw ParseException("Unparseable date: '$timeStampAsStr'", 0)
+    }
+
+    fun getType(json: JsonObject?): String {
+        val type = json?.lookup<String?>("message.@type")?.get(0)
+
+        if (type != null) {
+            return type
+        }
+
+        logger.warn("No @type in message so using default", "default_type" to typeDefault)
+        return typeDefault
+    }
+
+    fun generateKeyFromRecordBody(body: JsonObject?): ByteArray {
+        val id: JsonObject? = body?.let { getId(it) }
+        return if (id == null) ByteArray(0) else generateKey(id)
+    }
+
+    fun getId(json: JsonObject): JsonObject? {
+        val message = json["message"]
+        if (message != null && message is JsonObject) {
+            val id = message["_id"]
+
+            if (id != null) {
+                when (id) {
+                    is JsonObject -> {
+                        return id
+                    }
+                    is String -> {
+                        val idObject = JsonObject()
+                        idObject["id"] = id
+                        return idObject
+                    }
+                    is Int -> {
+                        val idObject = JsonObject()
+                        idObject["id"] = "$id"
+                        return idObject
+                    }
+                    else -> {
+                        return null
+                    }
+                }
+            }
+            else {
+                return null
+            }
+
+        }
+        else {
+            return null
+        }
+    }
+
+    fun parseJson(line: String?): JsonObject {
+        val parser: Parser = Parser.default()
+        val stringBuilder = StringBuilder(line)
+        return parser.parse(stringBuilder) as JsonObject
+    }
+
+    fun generateKey(json: JsonObject): ByteArray {
+        val jsonOrdered = sortJsonByKey(json)
+        val checksumBytes: ByteArray = generateFourByteChecksum(jsonOrdered)
+
+        return checksumBytes.plus(jsonOrdered.toByteArray())
+    }
+
+    fun sortJsonByKey(unsortedJson: JsonObject): String {
+        val sortedEntries = unsortedJson.toSortedMap(compareBy { it })
+        val json = JsonObject(sortedEntries)
+        return json.toJsonString()
+    }
+
+    fun sortJsonStringByKey(unsortedJson: String): String {
+        val parser: Parser = Parser.default()
+        val stringBuilder = StringBuilder(unsortedJson)
+        val jsonObject = parser.parse(stringBuilder) as JsonObject
+        val sortedEntries = jsonObject.toSortedMap(compareBy { it })
+        val jsonSorted: JsonObject = JsonObject(sortedEntries)
+        return jsonSorted.toJsonString()
+    }
+
+    fun generateFourByteChecksum(input: String): ByteArray {
+        val bytes = input.toByteArray()
+        val checksum = CRC32()
+
+        checksum.update(bytes, 0, bytes.size)
+
+        return ByteBuffer.allocate(4).putInt(checksum.value.toInt()).array()
+    }
+}
